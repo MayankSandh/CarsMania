06e96b448bb759ff9ffc61fa466f1493eee61ef5
Mayank Sandh <mayanksandh@gmail.com>
2024-01-12 20:20:24 +0530

    Added radars and data collection implemented

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ class Car(pygame.sprite.Sprite):
     def update(self):
         self.drive()
         self.rotate()
+        self.radar()
 
     def drive(self):
         if self.isDriving:
@@ -34,6 +35,31 @@ class Car(pygame.sprite.Sprite):
         self.vel_vector.rotate_ip(self.direction*self.rotation_vel)
         self.image = pygame.transform.rotozoom(self.original_image, self.angle, 0.1)
         self.rect = self.image.get_rect(center=self.rect.center)
+
+    def radar(self, radar_angle):
+        length = 0
+        x = int(self.rect.center[0])
+        y = int(self.rect.center[1])
+
+        while not SCREEN.get_at((x, y)) == pygame.Color(2, 105, 31, 255) and length < 200:
+            length += 1
+            x = int(self.rect.center[0] + math.cos(math.radians(self.angle + radar_angle)) * length)
+            y = int(self.rect.center[1] - math.sin(math.radians(self.angle + radar_angle)) * length)
+
+        # Draw Radar
+        pygame.draw.line(SCREEN, (255, 255, 255, 255), self.rect.center, (x, y), 1)
+        pygame.draw.circle(SCREEN, (0, 255, 0, 0), (x, y), 3)
+
+        dist = int(math.sqrt(math.pow(self.rect.center[0] - x, 2)
+                             + math.pow(self.rect.center[1] - y, 2)))
+
+        self.radars.append([radar_angle, dist])
+
+    def data(self):
+        input = [0, 0, 0, 0, 0]
+        for i, radar in enumerate(self.radars):
+            input[i] = int(radar[1])
+        return input
 
 car = pygame.sprite.GroupSingle(Car())
 
